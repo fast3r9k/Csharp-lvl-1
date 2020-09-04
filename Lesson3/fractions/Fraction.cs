3ba8c8f0768729b0e535ccb4795d1705a1438ea3
Nikita <faster20092009@yandex.ru>
2020-09-05 00:04:27 +0300

    add task 3

---
+++ Lesson3/fractions/Fraction.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fractions
+{
+    class Fraction
+    {
+        private int numerator;
+        private int denominator;
+        public int Numerator
+        {
+            get { return Numerator; }
+            set { Numerator = value; }
+        }
+        public int Denominator
+        {
+            get { return denominator; }
+            set 
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("Знаменатель не может быть равен 0");
+                }
+                else
+                {
+                    denominator = value;
+                }
+            }
+        }        
+
+
+        public Fraction()
+        {
+            numerator = 1;
+            denominator = 1;
+        }
+
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.Denominator = denominator;
+        }
+        static public Fraction operator +(Fraction f1, Fraction f2)
+        {
+            int df = f1.denominator;
+            int nf = f1.numerator + f2.numerator;
+            if (f1.denominator != f2.denominator)
+            {
+                df *= f2.denominator;
+                nf = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+            }
+            return new Fraction(nf, df);
+        }
+        static public Fraction operator -(Fraction f1, Fraction f2)
+        {
+            int df = f1.denominator;
+            int nf = f1.numerator - f2.numerator;
+            if (f1.denominator != f2.denominator)
+            {
+                df *= f2.denominator;
+                nf = f1.numerator * f2.denominator - f2.numerator * f1.denominator;
+
+            }
+            return new Fraction(nf, df);
+        }
+
+        static public Fraction operator *(Fraction f1, Fraction f2)
+        {
+
+            return new Fraction(f1.numerator * f2.numerator, f1.denominator * f2.denominator);
+        }
+
+        static public Fraction operator /(Fraction f1, Fraction f2)
+        {
+            if (f2.denominator == 0) throw new DivideByZeroException("Деление на 0");
+            else return new Fraction((f1.numerator * f2.denominator) , (f1.denominator*f2.numerator));
+        }
+        public override string ToString()
+        {
+            return (numerator <0 && denominator<0)?$"{-numerator} / {-denominator}": $"{numerator} / {denominator}";
+
+        }
+
+    }
+}
---
+++ Lesson3/fractions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fractions
+{
+    /*
+ * Описать класс дробей - рациональных чисел, являющихся отношением двух целых чисел. 
+ * Предусмотреть методы сложения, вычитания, умножения и деления дробей. 
+ * Написать программу, демонстрирующую все разработанные элементы класса. 
+ * Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение ArgumentException("Знаменатель не может быть равен 0");
+ * 
+ * Матвеев Никита
+ */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите числитель первой дроби: ");
+            int n1 = GetInt();
+            Console.Write("Введите знаменатель первой дроби: ");
+            int d1 = GetInt();
+            Fraction f1 = new Fraction(n1, d1);
+            Console.Write("Введите числитель второй дроби: ");
+            int n2 = GetInt();
+            Console.Write("Введите знаменатель второй дроби: ");
+            int d2 = GetInt();
+            Fraction f2 = new Fraction(n2, d2);
+            Fraction f3 = f1 + f2;
+            Fraction f4 = f1 - f2;
+            Fraction f5 = f1 * f2;
+            Fraction f6 = f1 / f2;
+            Console.WriteLine($"Результат сложения = {f3}\nРезультат вычитания = {f4}\nРезультат умножения = {f5}\nРезультат деления = {f6}");
+            Console.ReadLine();
+
+        }
+
+        static int GetInt()
+        {
+            while (true)
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                    Console.Write("Требуется числовое значение.\nПовторите ввод: ");
+                else return x;
+        }
+    }
+}
