3e9e3743257c36459225d19edc4fbc914b36956b
Nikita <faster20092009@yandex.ru>
2020-09-10 00:59:38 +0300

    add task 1 and 2

---
+++ Lesson5/Task2/Message.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    static class Message
+    {
+        static public string text;
+        static public string filename = "TEXT.txt";
+
+        static Message()
+        {            
+            LoadFromTxt(filename);
+        }
+        
+        static public void GetWordsByLength(int length)
+        {
+            Console.WriteLine($"Все слова короче {length} символов");
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '!', '?' });
+            foreach (var word in words)
+            {
+                if ( word != "")
+                {
+                    if (word.Length <= length)
+                        Console.WriteLine(word);
+                }
+            }
+        }
+
+        static public void DeleteWordByEnd(string symbol)
+        {
+
+            Console.WriteLine($"\nРезультат после удаления слов, заканчивающихся на {symbol} :");
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '!', '?' });
+            for (int i = 0; i < words.Length;i++)
+            {
+                if (words[i].EndsWith(symbol.ToLower()))
+                {
+                    words[i] = null;
+                }
+                if (words[i] != null)
+                    Console.Write($"{words[i]} ");
+            }         
+        }
+
+        static public string TheLongestWord()
+        {            
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '!', '?' });
+            string temp = words[0];
+            int max = 0;
+            foreach(var word in words)
+            {
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                    temp = word;
+                }
+            }
+            
+            return temp;
+        }
+
+        static public StringBuilder GetLongStringBuild()
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            StringBuilder result = new StringBuilder();
+            int max = Message.TheLongestWord().Length;
+            foreach (string word in words)
+            {
+                if (word.Length == max)
+                {
+                    result.Append(word + " ");
+                }
+            }
+            return result;
+        }
+
+        static public string LoadFromTxt(string filename)
+        {
+            filename = @"..\..\" + filename;
+            using (StreamReader sr = new StreamReader(filename, Encoding.Default))
+            {
+                text = sr.ReadLine();
+                sr.Close();
+                return text;
+            }
+        }
+    }
+}
---
+++ Lesson5/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Message.GetWordsByLength(7);
+            Message.DeleteWordByEnd("и");
+            Console.WriteLine($"\nСамое длинное слово - { Message.TheLongestWord()}");
+            Console.WriteLine($"{Message.GetLongStringBuild()}");
+            Console.ReadLine();
+        }
+    }
+}
