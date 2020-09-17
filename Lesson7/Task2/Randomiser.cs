1a80a715ac1ca6632cb3b90e1ff00f77ed6ae9c0
Nikita <faster20092009@yandex.ru>
2020-09-17 22:57:54 +0300

    Add Homework 7

---
+++ Lesson7/Task2/Randomiser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Randomiser
+    {
+        int value;
+        int steps;
+
+        public int Value { get { return this.value; } }
+        public int Steps { get { return this.steps; } }
+
+        public Randomiser(int max)
+        {
+            Start(max);
+        }
+
+        public void Start(int max)
+        {
+            Random r = new Random();
+            this.value = r.Next(0, max);
+        }
+
+        public string Check(out bool check, int userAnswer)
+        {
+            if(userAnswer == this.value)
+            {
+                check = true;
+                steps++;
+                return $"Вы угадали число. Совершено попыток = {steps}";
+            }    
+            else if(userAnswer < this.value)
+            {
+                check = false;
+                steps++;
+                return $"Ваше число слишком маленькое";
+            }
+            else
+            {
+                check = false;
+                steps++;
+                return $"Ваше число слишком большое";
+            }
+        }
+        public void Reset()
+        {
+            this.steps = 0;
+        }
+    }
+}
